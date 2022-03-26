918f58a6d5386645a6452ee43de62f23f554541c
Jaydeep Ravat <47079969+Jaydeep45@users.noreply.github.com>
2022-03-26 18:29:40 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+    include_once "connection.php";
+    $con = Connection::connect();
+    session_start();
+    header('location:home.php');
+    if(isset($_POST['login'])) {
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_psw'];
+        $sql = "select * from users where user_email = :uemail";
+        $statement = $con->prepare($sql);
+        $statement->execute([
+            ':uemail'=> $user_email
+        ]);
+        $data = $statement->fetch(PDO::FETCH_ASSOC);
+        if($statement->rowCount()) {
+            $hash_password = $data['user_password'];
+            if(!password_verify($user_password, $hash_password)) {
+                header('location:index.php');
+            }
+            else {
+                $_SESSION['user_email'] = $user_email;
+            }
+        }
+        else {
+            header('location:index.php');
+        }
+    }
